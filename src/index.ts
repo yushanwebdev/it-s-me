dcbd3a5fb1ffbbe9906572d9abd1b6a7a24c1aa5
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-03 17:07:16 +0530

    feat: MongoDB Setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -20,3 +21,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
 })
+
+const MONGO_URL = 'mongodb+srv://yushanwebdev:HdU8SJZpJKU8tfVM@cluster0.v8xa6kb.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));

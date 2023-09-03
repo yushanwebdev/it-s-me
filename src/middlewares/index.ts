f793a28caf6b453a4c84f3ffef4a4c3804876324
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-03 20:59:51 +0530

    feat: User controller and routing

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,30 @@
 import { getUserBySessionToken } from "../db/user";
 import express from "express";
-import { merge } from "lodash";
+import { get, merge } from "lodash";
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log("error", error);
+    return res.sendStatus(400);
+  }
+};
 
 export const isAuthenticated = async (
   req: express.Request,

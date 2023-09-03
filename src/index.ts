cd9753b1d6cdf4c6fa47fb6edc134fadadc11f50
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-03 17:39:09 +0530

    feat: Creating user model

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.use(cors({
-    credentials: true
-}))
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
@@ -19,11 +21,12 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');
-})
+  console.log("Server is running on http://localhost:8080");
+});
 
-const MONGO_URL = 'mongodb+srv://yushanwebdev:HdU8SJZpJKU8tfVM@cluster0.v8xa6kb.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL =
+  "mongodb+srv://yushanwebdev:HdU8SJZpJKU8tfVM@cluster0.v8xa6kb.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error));
+mongoose.connection.on("error", (error: Error) => console.log(error));

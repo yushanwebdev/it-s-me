a9c7804e59ffd7da9d59f04057721f890e8606fc
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-03 20:02:10 +0530

    feat: Middleware creation

---
+++ src/middlewares/index.ts
@@ -0,0 +1,30 @@
+import { getUserBySessionToken } from "../db/user";
+import express from "express";
+import { merge } from "lodash";
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["YUSHAN-SESSION-TOKEN"];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log("error", error);
+    return res.sendStatus(400);
+  }
+};
